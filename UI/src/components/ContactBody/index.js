a938977c4b596e20b5e5b975d5cdf7aed80544d8
Krishna Devashish <itskd17@gmail.com>
2024-12-26 10:59:42 +0530

    Added email js to send contact details

--- UI/src/components/ContactBody/index.js
+++ UI/src/components/ContactBody/index.js
@@ -1,9 +1,14 @@
-import { Button, Form, Input, Select } from "antd";
+import { Button, Form, Input, Select, Spin } from "antd";
 import HOSPITAL from "../../assets/images/hospital.png";
 import { Option } from "antd/es/mentions";
 import { SendOutlined } from "@ant-design/icons";
+import emailjs from "emailjs-com";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const ContactBody = () => {
+	const [isSending, setIsSending] = useState(false);
+	const [contactform] = Form.useForm();
 	const validateMessages = {
 		required: "${label} is required!",
 		types: {
@@ -14,8 +19,44 @@ const ContactBody = () => {
 			range: "${label} must be between ${min} and ${max}",
 		},
 	};
-	const onFinish = (values) => {
-		console.log(values);
+
+	const handleSubmit = (values) => {
+		const USER_ID = process.env.REACT_APP_EMAILJS_USER_ID;
+		const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+		const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+		const REPLY_ID = process.env.REACT_APP_REPLY_MAIL_ID;
+
+		if (!USER_ID || !SERVICE_ID || !TEMPLATE_ID) {
+			return console.error(
+				"EmailJS user ID or service ID or template ID not found",
+			);
+		} else {
+			setIsSending(true);
+			emailjs.init(USER_ID);
+			const templateParams = {
+				name: `${values.user.firstName} ${values.user.lastName}`,
+				email: values.user.email,
+				reply_to: REPLY_ID,
+				message: `The contact issue is realted to ${values.user.topic} \n\n The reason for contact is \n ${values.user.message} \n Phone: ${values.user.phone}\n`,
+			};
+			emailjs
+				.send(SERVICE_ID, TEMPLATE_ID, templateParams)
+				.then((response) => {
+					console.log(
+						"Email sent successfully!",
+						response.status,
+						response.text,
+					);
+					setIsSending(false);
+					contactform.resetFields();
+					toast.success("Email sent successfully!");
+				})
+				.catch((err) => {
+					setIsSending(false);
+					toast.error("Failed to send email!");
+					console.error("Failed to send email:", err);
+				});
+		}
 	};
 	return (
 		<>
@@ -31,10 +72,11 @@ const ContactBody = () => {
 				</div>
 				<Form
 					name='contactForm'
-					onFinish={onFinish}
+					onFinish={handleSubmit}
 					validateMessages={validateMessages}
 					className='doc-app__contact-form'
 					layout='vertical'
+					form={contactform}
 				>
 					<div className='doc-app__contact-form-section'>
 						<Form.Item
@@ -100,11 +142,11 @@ const ContactBody = () => {
 							]}
 						>
 							<Select placeholder='Select one...'>
-								<Option value='treatment'>Treatment</Option>
-								<Option value='payment'>Payment</Option>
-								<Option value='doctor'>Doctor</Option>
-								<Option value='appointment'>Appointment</Option>
-								<Option value='general'>General</Option>
+								<Option value='Treatment'>Treatment</Option>
+								<Option value='Payment'>Payment</Option>
+								<Option value='Doctor'>Doctor</Option>
+								<Option value='Appointment'>Appointment</Option>
+								<Option value='General'>General</Option>
 							</Select>
 						</Form.Item>
 					</div>
@@ -127,13 +169,15 @@ const ContactBody = () => {
 					</div>
 					<div className='doc-app__contact-form-section'>
 						<Form.Item label={null}>
-							<Button type='primary' htmlType='submit' size="large">
+							<Button type='primary' htmlType='submit' size='large'>
 								Send Message <SendOutlined />
 							</Button>
 						</Form.Item>
 					</div>
 				</Form>
 			</div>
+			<Spin spinning={isSending} tip='Loading' size='large' fullscreen />
+			<ToastContainer />
 		</>
 	);
 };
